fc619057e54eb692b9b8a36a203cce75bc4c8481
DmytroKuzmenko <kuzmenko.dm@gmail.com>
2025-04-18 21:07:28 +0200

    revert fixes Try to fix top margin mobile

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,4 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-  function setVh() {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }
-  setVh();
-  window.addEventListener('resize', setVh);
+
