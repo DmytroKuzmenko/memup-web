e563fc635187bef3eb7f8e1730c4aea07e219939
DmytroKuzmenko <kuzmenko.dm@gmail.com>
2025-04-18 19:59:21 +0200

    Try to fix top margin mobile 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,9 @@ export class AppComponent {
 
 }
 
-function updateVh() {
+function setVh() {
   const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
-
-window.addEventListener('resize', updateVh);
-updateVh(); // вызываем при загрузке
+setVh();
+window.addEventListener('resize', setVh);
