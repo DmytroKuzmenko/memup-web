e3622ebbd565b323d5e88cb255901faceac9d2d4
DmytroKuzmenko <kuzmenko.dm@gmail.com>
2025-04-18 19:33:49 +0200

    Try to fix top margin mobile 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,3 +11,11 @@ export class AppComponent {
   title = 'memeup';
 
 }
+
+function updateVh() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('resize', updateVh);
+updateVh(); // вызываем при загрузке
