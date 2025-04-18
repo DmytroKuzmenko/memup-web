ca02859fd8081ae59e8571c7f5506d13fa1f884b
DmytroKuzmenko <kuzmenko.dm@gmail.com>
2025-04-18 17:06:32 +0200

    Fix PWA 1

--- src/app/pages/meme-detail/meme-detail.component.ts
+++ src/app/pages/meme-detail/meme-detail.component.ts
@@ -12,7 +12,7 @@ import { RouterModule } from '@angular/router';
 export class MemeDetailComponent {
   meme = {
     text: 'Ich liebe Montag...',
-    translation: 'Я люблю понеділок...',
+    translation: 'Я люблю понеділок... (test)',
     example: 'Zum Beispiel: Ich liebe Montag, weil ich produktiv bin.',
     note: 'Цей мем іронічний — більшість людей не люблять понеділки.'
   };
