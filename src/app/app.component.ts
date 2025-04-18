32274cd2e3a1b632ac39d9f88b0e26bcdbaf41cf
DmytroKuzmenko <kuzmenko.dm@gmail.com>
2025-04-18 20:16:32 +0200

    Try to fix top margin mobile 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,3 @@ export class AppComponent {
 
 }
 
-function setVh() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-setVh();
-window.addEventListener('resize', setVh);
--- src/app/pages/meme-detail/meme-detail.component.ts
+++ src/app/pages/meme-detail/meme-detail.component.ts
@@ -12,7 +12,7 @@ import { RouterModule } from '@angular/router';
 export class MemeDetailComponent {
   meme = {
     text: 'Ich liebe Montag...',
-    translation: 'Я люблю понеділок... (test3)',
+    translation: 'Я люблю понеділок... (test4)',
     example: 'Zum Beispiel: Ich liebe Montag, weil ich produktiv bin.',
     note: 'Цей мем іронічний — більшість людей не люблять понеділки.'
   };
