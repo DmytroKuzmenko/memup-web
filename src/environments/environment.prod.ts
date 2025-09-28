db24fab31f8f3750a335877ee367d8867845b7ed
DmytroKuzmenko <kuzmenko.dm@gmail.com>
2025-09-28 14:00:30 +0200

    -fix api prod proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // В проде Nginx/Traefik тоже проксируют /api → memeup-api
-  apiBaseUrl: 'https://memeup-api.kuzmenko.dev/api',
+  apiBaseUrl: '/api',
 };
